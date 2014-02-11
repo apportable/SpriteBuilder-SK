e58ea690339bec600051a7ecfbbe639594041e61
Steffen Itterheim <steffen@learn-cocos2d.com>
2014-02-11 20:21:18 +0100

    added CCScheduler class

---
+++ CCBReader+SpriteKitExtensions/cocos2d-classes/ccTypes.h
@@ -0,0 +1,25 @@
+//
+//  ccTypes.h
+//  SB+KoboldKit
+//
+//  Created by Steffen Itterheim on 11/02/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#ifndef SB_KoboldKit_ccTypes_h
+#define SB_KoboldKit_ccTypes_h
+
+typedef double CCTime;
+
+#import "KKNode.h"
+#import "KKScene.h"
+#import "KKSpriteNode.h"
+
+typedef KKNode CCNode;
+typedef KKScene CCScene;
+typedef KKSpriteNode CCSprite;
+typedef SKTexture CCSpriteFrame;
+typedef SKTexture CCTexture;
+typedef SKPhysicsBody CCPhysicsBody;
+
+#endif
